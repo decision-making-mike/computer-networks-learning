dabcac87ea57bd9acde83185727210650f9b1447
decision-making-mike <183248904+decision-making-mike@users.noreply.github.com>
2025-01-04 04:25:27 +0100

    RDT simulation ACK packets loss implementation, refactoring, and TODO addition

--- rdt-simulation/run.sh
+++ rdt-simulation/run.sh
@@ -15,6 +15,11 @@ transport_packet () {
     payload="$1"
     # Can there be any problems in Bash with variables having the same names as built-ins?
     type="$2"
+    lost="$3"
+
+    if [[ "$lost" -eq 1 ]]
+    then return 1
+    fi
 
     for (( x = 0 ; x < "$height" ; ++x ))
     do
@@ -30,6 +35,19 @@ transport_packet () {
     done
 }
 
+send_data_packet () {
+    echo -ne '\033[K'
+    echo "${data:$(( index + 1 )):${#data}}"
+}
+
+send_ack_packet () {
+    echo -ne '\r'
+    echo -ne '\033[K'
+    echo -n "${data:0:$(( index + 1 ))}"
+    echo -ne "\033[1A"
+    echo -ne '\r'
+}
+
 # I could also put "trap" before "tput". But then the user could be left with a trap of no purpose anymore if they issued "CTRL-C" between "trap" and "tput". Remarkable that they would not see the trap. I of course assume them to read the script before executing it, but nonetheless the fact of having the trap set could be overlooked. To avoid this problem, I have put "tput" before "trap". Now issuing "CTRL-C" in the aforementioned moment shall not left the trap. And as far as it then shall make the cursor invisible, such a change will be visible for the user, unlike the trap.
 # One could say, "if you use "tput" anyway, why not also moving the cursor? Now if the user issues "CTRL-C" when the script is in the middle of transporting the payload, they are left with messy character leftover". Well, true that they are left so, but making so much cleanup feels to me like there would be done too much on the script side. Here I would rather do just the minimum for the script to run properly.
 tput 'civis'
@@ -40,22 +58,24 @@ echo -n "$data"
 echo -ne '\r'
 
 sleep 1
-for (( index = 0 ; index < "${#data}" ; ++index ))
+ack_received=1
+for (( index = 0 ; index < "${#data}" ; ))
 do
-    echo -ne '\033[K'
-    echo "${data:$(( index + 1 )):${#data}}"
+    # TODO Actual ACK awaiting implementation (read -t?)
+    {
+        send_data_packet \
+            && transport_packet "${data:$index:1}" '' 0 \
+            && send_ack_packet \
+            && transport_packet '0' 'ACK' "(( RANDOM % 2 ))" \
+            && ack_received=1
+    } || ack_received=0
 
-    # Transport the message with actual data.
-    transport_packet "${data:$index:1}" ''
-
-    echo -ne '\r'
-    echo -ne '\033[K'
-    echo -n "${data:0:$(( index + 1 ))}"
-    echo -ne "\033[1A"
-    echo -ne '\r'
-
-    # Transport the "ACK" message.
-    transport_packet '0' 'ACK'
+    if [[ "$ack_received" -eq 1 ]]
+    then index="$(( index + 1 ))"
+    else
+        # We jump straight to the sender, and not a fixed number of lines, because we assume that we cannot know in which line the packet has been lost.
+        echo -ne "\033[;H"
+    fi
 done
 
 echo -ne "\033[$(( height + 2 ))B"
